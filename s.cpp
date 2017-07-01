a34fa3ad4cc0fc0d10a98f594bc7120bcca26771
성지현 <seongjihyeon@seongjihyeon-ui-MacBook-Pro.local>
2017-07-01 23:51:45 +0900

    a

---
+++ s.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int arr[11] = {0};
+
+	cout<<arr[0]<<endl;
+	
+	return 0;
+}
+
--- stick.cpp
+++ stick.cpp
@@ -2,23 +2,30 @@
 #include <algorithm>
 using namespace std;
 
+int search(int *arr,int n);
+
+
 int main()
 {
 	int arr[11] = {0,1,5,8,9,10,17,17,20,24,30};
-
+	cout<<search(arr,7)<<endl;
 	return 0;
 }
 
 int search(int * arr, int n)
 {
 	int result = -1;
-	int sol[11] = {0};
+	int sol[11] = {0,1};
+	int q = 0;
 
 	for(int i = 1 ;i <= n ; i++)
-	{
+	{	
+		q = arr[i];
+
 		for(int j = 1 ; j <= i ; j++)
 		{
-			result = max(arr[i],sol[j-1]+arr[j-1]);
+			q = max(q,arr[j]+sol[i-j]);
+			result = max(q,result);		
 		}
 
 		sol[i] = result;
